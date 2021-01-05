21d1f87c0e0e6b36a6086f944130fdceae7a3769
jaydeep <jaydeep_gajera13051993@yahoo.com>
2021-01-05 16:57:44 +0530

    a

---
+++ px.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var abp=abp||false;var scripts=document.getElementsByTagName("script");var script=scripts[scripts.length-1];if(script){var query=script.src.replace(/^[^\?]+\??/,"").split("&");var params={};for(var i=0;i<query.length;i++){var param=query[i].split("=");params[param[0]]=param[1]}if(params["ch"]==1)abp=true;else if(params["ch"]==2)abp=abp&&false};
+
+}
+/*
+     FILE ARCHIVED ON 01:39:57 Oct 23, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 08:59:16 Jan 05, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  PetaboxLoader3.datanode: 96.065 (4)
+  load_resource: 109.692
+  captures_list: 157.485
+  exclusion.robots: 0.18
+  PetaboxLoader3.resolve: 67.89 (2)
+  LoadShardBlock: 131.071 (3)
+  CDXLines.iter: 22.51 (3)
+  esindex: 0.011
+  RedisCDXSource: 0.737
+  exclusion.robots.policy: 0.168
+*/
